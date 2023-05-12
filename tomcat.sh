6fdba3ff1ab5bc5e40862e4d3faf11a18f01469c
Rajesh B T <103110767+Raj000007@users.noreply.github.com>
2023-05-12 14:34:27 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -2,7 +2,7 @@
 sudo su -
 cd /tmp
 yum install -y java
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.68/bin/apache-tomcat-9.0.68.tar.gz
-tar -xf apache-tomcat-9.0.68.tar.gz
-sh apache-tomcat-9.0.68/bin/startup.sh
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.75/bin/apache-tomcat-9.0.75.tar.gz
+tar -xf apache-tomcat-9.0.75.tar.gz
+sh apache-tomcat-9.0.75/bin/startup.sh
 
